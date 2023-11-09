bbfba18dea71b8b3691378c53fe6bcbbe44b312e
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-11-09 00:17:56 -0300

    adição da 2ª aula sobre requisição ajax (xml2)

--- exercicios-web/ajax/server.js
+++ exercicios-web/ajax/server.js
@@ -1,9 +1,32 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 const app = express();
+// const multer = require('multer');
+import multer from 'multer';
 
 app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload');
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.send('Erro no Upload...');
+        }
+
+        res.end('Upload concluído!')
+    })
+});
 
 app.listen(3333, () => console.log('Executando'));
